c7e7ad06074e4c5c9f276e67d80e3298fa4753ad
wwt3100 <309246295@qq.com>
2018-05-03 18:48:41 +0800

    加入工程模式

--- app/src/main/java/com/a9klab/nanopi3serialport/MainActivity.kt
+++ app/src/main/java/com/a9klab/nanopi3serialport/MainActivity.kt
@@ -10,22 +10,30 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 import com.friendlyarm.AndroidSDK.HardwareControler
 import com.a9klab.nanopi3serialport.SerialPort
-import android.widget.EditText
-import android.widget.TextView
 import java.util.*
 import android.view.View.FOCUS_DOWN
-import android.widget.Button
-import android.widget.ScrollView
 import java.nio.file.Files.delete
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView.OnEditorActionListener
+import android.app.Activity
+import android.opengl.Visibility
+import android.text.TextWatcher
+import android.view.KeyEvent
+import android.widget.*
+import android.text.Editable
+import android.R.id.edit
+import android.content.Context
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 
 
-class MainActivity : AppCompatActivity(), DataListener {
+class MainActivity : AppCompatActivity(), DataListener, View.OnClickListener {
     private val TAG = "SerialPort"
     private var fromTextView: TextView? = null
     private var toEditor: EditText? = null
     private val MAXLINES = 200
     private val remoteData = StringBuilder(256 * MAXLINES)
-
+    private var mInputMethodManager: InputMethodManager? = null
     private val devName = "/dev/ttySAC4"
     private var speed: Long = 115200
     private var dataBits = 8
@@ -39,25 +47,68 @@ class MainActivity : AppCompatActivity(), DataListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
-
-        button2.setOnClickListener {
-            textView.text = "123"
+        mInputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        devfd = gSerialPort.InitComm()
+        if (devfd >= 0) {
+            //Log.d(TAG, "Open Success")
+            SerialPort.Start()
+        } else {
+            devfd = -1
+            Toast.makeText(this, "串口未打开", Toast.LENGTH_LONG).show()
+            btn_conn.visibility = View.VISIBLE
+            Log.d(TAG, "Fail to open " + devName + "!")
+        }
+        btn_conn.setOnClickListener {
             devfd = gSerialPort.InitComm()
             if (devfd >= 0) {
                 //Log.d(TAG, "Open Success")
                 SerialPort.Start()
+                btn_conn.visibility = View.GONE
             } else {
                 devfd = -1
+                Toast.makeText(this, "串口未打开", Toast.LENGTH_LONG).show()
                 Log.d(TAG, "Fail to open " + devName + "!")
             }
         }
-        btn_send.setOnClickListener {
-            val b = byteArrayOf(0xAA.toByte(), 0xBB.toByte(), 0x0d.toByte(), 0x0a.toByte())
-            txv_send.text = SerialPort.SendEx(b).toString()
+//        t_left_freq.setOnClickListener {
+//            t_left_freq.setText("")
+//        }
+//        t_right_freq.setOnClickListener {
+//            t_right_freq.setText("")
+//        }
+        t_left_freq.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                t_left_freq.setText("")
+            } else {
+                t_left_freq.setText("3.0")
+            }
         }
+//        t_left_freq.addTextChangedListener(object : TextWatcher {
+//            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {}
+//            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int,
+//                                           after: Int) {
+//            }
+//
+//            override fun afterTextChanged(s: Editable) {
+//                t_left_freq.setFocusable(false);//设置输入框不可聚焦，即失去焦点和光标
+//            }
+//        })
+//        t_right_freq.addTextChangedListener(object : TextWatcher {
+//            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int,
+//                                           after: Int) {
+//            }
+//
+//            override fun afterTextChanged(s: Editable) {
+//                t_right_freq.setFocusable(false);//设置输入框不可聚焦，即失去焦点和光标
+//            }
+//        })
+//        btn_send.setOnClickListener {
+//            val b = byteArrayOf(0xAA.toByte(), 0xBB.toByte(), 0x0d.toByte(), 0x0a.toByte())
+//            txv_send.text = SerialPort.SendEx(b).toString()
+//        }
     }
 
     /**
@@ -70,10 +121,11 @@ class MainActivity : AppCompatActivity(), DataListener {
         override fun handleMessage(msg: Message) {
             //更新UI
             when (msg.what) {
-                1 -> textView.text = "345"
+            //1 -> textView.text = "345"
             }
         }
     }
+
 
     override fun getData(data: ByteArray?) {
         if (data == null) {
@@ -92,8 +144,8 @@ class MainActivity : AppCompatActivity(), DataListener {
 //        cmd=cmd.shl(8)
 //        cmd=cmd or data[2].toInt()
         val message = Message()
-       when ((data[1] .toInt() shl 8) or data[2].toInt()) {
-       //when (cmd) {
+        when ((data[1].toInt() shl 8) or data[2].toInt()) {
+        //when (cmd) {
             0x0101 -> {
                 //TODO: 事件驱动,不能直接改写
                 message.what = 1
@@ -116,5 +168,23 @@ class MainActivity : AppCompatActivity(), DataListener {
         }
     }
 
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.t_left_freq//输入框
+            -> {
+                t_left_freq.setFocusable(false)//设置输入框不可聚焦，即失去焦点和光标
+                if (mInputMethodManager!!.isActive) {
+                    mInputMethodManager!!.hideSoftInputFromWindow(t_left_freq.windowToken, 0)// 隐藏输入法
+                }
+            }
+            R.id.t_right_freq//确定按钮
+            -> {
+                t_right_freq.setFocusable(false)//设置输入框不可聚焦，即失去焦点和光标
+                if (mInputMethodManager!!.isActive) {
+                    mInputMethodManager!!.hideSoftInputFromWindow(t_right_freq.windowToken, 0)// 隐藏输入法
+                }
+            }
+        }
+    }
 
 }
